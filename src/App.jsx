eac0f3107f82186c4f7247e312d989e950a4fc3c
Ezequiel Gonzalez <88798247+ezexgonzalez@users.noreply.github.com>
2026-09-14 01:02:17 -0300

    feat: render training section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from './components/layout/Navbar'
 import { HeroSection } from './components/sections/HeroSection'
 import { MethodSection } from './components/sections/MethodSection'
+import { TrainingSection } from './components/sections/TrainingSection'
 import { siteConfig } from './data/siteConfig'
 import { createThemeStyles } from './utils/themeStyles'
 
@@ -19,6 +20,7 @@ function App() {
       <main id="main-content" tabIndex="-1">
         <HeroSection />
         <MethodSection />
+        <TrainingSection />
       </main>
     </div>
   )
